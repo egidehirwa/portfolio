3862bbca64ea61607241a721aefa35e242a3cdc7
egidehirwa <egidehirwam@gmail.com>
2023-01-13 23:56:04 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Intro from "./components/Intro";
+import Portfolio from "./components/portfolio";
+import Contact from "./components/Contact";
+import Timeline from "./components/Timeline";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+      <Portfolio />
+      <Timeline />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "Inyange Asbl",
+    imgUrl: "",
+    stack: ["WordPress", "Elementor"],
+    link: "https://inyange.com/",
+  },
+  {
+    title: "House of venus",
+    imgUrl: "",
+    stack: ["Webflow"],
+    link: "https://house-of-venus.webflow.io/",
+  },
+  {
+    title: "Landing Page",
+    imgUrl: "",
+    stack: ["HTML", "TailwindCSS", "JavaScript", "Vite"],
+    link: "https://egidehirwa.github.io/tailwindcss-landing-page/",
+  },
+  {
+    title: "Developer Portfolio",
+    imgUrl: "",
+    stack: ["React", "TailwindCSS", "Vite"],
+    link: "https://egidecodes.pages.dev",
+  },
+];

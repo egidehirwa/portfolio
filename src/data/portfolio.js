bb1c3e5581e568189050322b8eb64a05959d49d2
egidehirwa <egidehirwam@gmail.com>
2023-01-14 12:40:54 +0100

    Styled the portfolio section

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,26 @@
 export default [
   {
     title: "Inyange Asbl",
-    imgUrl: "",
+    imgUrl: "/assets/inyange-website.jpg",
     stack: ["WordPress", "Elementor"],
     link: "https://inyange.com/",
   },
   {
     title: "House of venus",
-    imgUrl: "",
+    imgUrl: "/assets/hov-website.jpg",
     stack: ["Webflow"],
     link: "https://house-of-venus.webflow.io/",
   },
-  /*   {
+  {
     title: "Landing Page",
-    imgUrl: "",
+    imgUrl: "public/assets/manage-website.jpg",
     stack: ["HTML", "TailwindCSS", "JavaScript", "Vite"],
     link: "https://egidehirwa.github.io/tailwindcss-landing-page/",
-  }, */
-  {
+  },
+  /*   {
     title: "Developer Portfolio",
-    imgUrl: "",
+    imgUrl: "src/assets/Manage landing page image thumbnail.jpg",
     stack: ["React", "TailwindCSS", "Vite"],
     link: "https://egidecodes.pages.dev",
-  },
+  }, */
 ];

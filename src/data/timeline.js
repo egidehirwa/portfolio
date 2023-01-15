267f836f255cbaa420aca92c0e0595ddf74b672e
egidehirwa <egidehirwam@gmail.com>
2023-01-15 23:54:34 +0100

    Adden timeline with details about my journey

---
+++ src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    year: "2022",
+    title: "FrontEnd Engineer",
+    duration: "5 months",
+    details:
+      "I have been doing this for quite some time and l would like to keep on doing it for as long as possible.",
+  },
+  {
+    year: "2020",
+    title: "Bartender",
+    duration: "3 years",
+    details:
+      "I have been doing this for quite some time and l would like to keep on doing it for as long as possible.",
+  },
+  {
+    year: "2015",
+    title: "Inyange Dancer",
+    duration: "7 years",
+    details:
+      "I have been doing this for quite some time and l would like to keep on doing it for as long as possible.",
+  },
+  {
+    year: "2009",
+    title: "SpeedCuber",
+    duration: "14 years",
+    details:
+      "I have been doing this for quite some time and l would like to keep on doing it for as long as possible.",
+  },
+];

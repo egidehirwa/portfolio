b46c01fb0abd040154dfd0f2a088f56816edaa1f
egidehirwa <egidehirwam@gmail.com>
2023-01-21 23:49:24 +0100

    feature: Added dynamic footer and section title

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import timeline from "../data/timeline";
 import TimelineItem from "./TimelineItems";
+import Title from "./Title";
 
 function Timeline() {
   return (
     <div className="flex flex-col md:flex-row justify-center my-20 mx-10">
       <div className="w-full md:w-7/12">
+        <Title>Timeline</Title>
         {timeline.map((item) => (
           <TimelineItem
             year={item.year}

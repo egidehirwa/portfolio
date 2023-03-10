ed0c5395e5c4c7c781aeb1d1f14cc14673f8a101
egidehirwa <egidehirwam@gmail.com>
2023-03-10 14:43:04 +0100

    Changed dev name back to Egide

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
-        Arsène Rurangiza
+        Egide Hirwa
       </h1>
       <p className="text-base md:text-xl mb-3 font-light">Web Developer</p>
       <p className="text-sm max-w-xs md:max-w-xl mb-6 font-medium ">
